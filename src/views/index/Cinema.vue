b9a16a1e07ca2ab7f8bbee32ff29cd4aefd3c22d
yshujun <771145708@qq.com>
2019-04-22 09:32:26 +0800

    失败的搜索

--- src/views/index/Cinema.vue
+++ src/views/index/Cinema.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
-    <p v-for="item in 1000" :key="item">我是一个屁</p>
+    <h1 class="header">影院</h1>
+    <form action="/">
+      <van-search
+        v-model="value"
+        placeholder="请输入搜索关键词"
+        show-action
+        @search="onSearch"
+        @cancel="onCancel"
+      />
+    </form>
   </div>
 </template>
+
+<script>
+
+export default {
+
+}
+</script>
