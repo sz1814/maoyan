d2dd1285d6cfb461d1203e24726094f8c3f57c1c
yshujun <771145708@qq.com>
2019-04-22 15:42:03 +0800

    粘住

--- src/views/index/Cinema.vue
+++ src/views/index/Cinema.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
     <h1 class="header">影院</h1>
+
     <form class="search" action="/">
-      <router-link to="../city" class="dizhi" @click="city">
+      <router-link to="../city" class="dizhi">
         <span>深圳</span>
         <van-icon name="arrow-down" />
       </router-link>
       <router-link to="../search">
         <van-search
         class="search-btn"
-        v-model="value"
         placeholder="搜影院"
         readonly="false"
         input-align="center"
@@ -18,15 +18,49 @@
       </router-link>
 
     </form>
+
+    <div class="nav">
+      <van-tabs line-height="0"
+      title-active-color="#e54847"
+       @click="onCkick"
+       >
+        <van-tab >
+            <div slot="title">
+              全城<van-icon name="arrow-down" size="8px" />
+            </div>
+            <div class="quancheng">
+              111
+            </div>
+        </van-tab>
+        <van-tab >
+            <div slot="title">
+              品牌<van-icon name="arrow-down" size="8px" />
+            </div>
+              222
+        </van-tab>
+        <van-tab >
+            <div slot="title">
+              特色<van-icon name="arrow-down" size="8px" />
+            </div>
+              333
+        </van-tab>
+      </van-tabs>
+    </div>
+
+    <div>
+      <ul>
+        <li v-for="item in 40">品牌</li>
+      </ul>
+    </div>
+
   </div>
 </template>
 
 <script>
 
 export default {
-  city () {
 
-  }
+
 }
 </script>
 
@@ -43,12 +77,15 @@ export default {
   overflow: hidden;
   background:#e54847;
   color:#fff;
+  position:fixed;
+  top:0;
 }
 .search{
   width:100%;
   background:#f5f5f5;
   height: 44px;
-
+  position:fixed;
+  top:50px;
   .dizhi{
     padding-left:15px;
     color:#666;
@@ -69,7 +106,16 @@ export default {
     border:.5px solid #e6e6e6;
   }
 }
-  element.style{
-    background: #f5f5f5 ;
+
+element.style{
+  background: #f5f5f5 ;
+}
+.nav{
+  width:100%;
+  position:fixed;
+  top:94px;
+  .van-tabs__content{
+    display:none;
   }
+}
 </style>
